c3c728a37df30597b6eb44c43ab438adb2c67b21
adonis97 <simoadonis@gmail.com>
2017-05-02 14:26:51 +0100

    after adding postgre support

--- app/settings.py
+++ app/settings.py
@@ -96,9 +96,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adonis_shop',
         'ATOMIC_REQUESTS': True,
+        'HOST':'127.0.0.1',
+        'USER':'adonis',
+        'PASSWORD':'programmeur',
+        'PORT':'5433'
     }
 }
 
